9d0da6738de0a2de6219505a026a15944c56c11f
dj-257 <akhildhyani420@gmail.com>
2025-01-31 16:44:43 +0530

    final

--- src/components/Team.js
+++ src/components/Team.js
@@ -31,6 +31,8 @@ export default function Team() {
         <Card name='Jimeet Shastri' image='jimeet' insta='jimeet2440' linkedin='jimeet-shastri-034761327' rno='b24cs1034'/>
         <Card name='Brijesh Thakkar' image='brijesh' insta='brijesh_thakkar__' linkedin='brijesh-thakkar-22912b290' rno='b24me1018'/>
         <Card name='Nirni Naidu' image='nirni' insta='proff__coder___' linkedin='nirni-naidu-696ba2202' rno='b24ee1075'/>
+        <Card name='Alankrita Singh' image='alankrita' insta='alankritaa.singh_' linkedin='' rno='b24mt1004'/>
+        <Card name='Vidhi Sharma' image='vidhi' insta='' linkedin='vidhi-sharma-009245331' rno='b24mt1045'/>
     </div>
 
 
